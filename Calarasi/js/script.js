4fbe66837bc53e7104d03342878ba7b4527a385e
Fedot <tudor.onceanu@gmail.com>
2021-08-18 11:56:21 +0300

    select ibucii

--- Calarasi/js/script.js
+++ Calarasi/js/script.js
@@ -272,17 +272,77 @@ e.exports=function(e){var t={};function i(n){if(t[n])return t[n].exports;var r=t
 
 const region = () => {
     const region = document.querySelector('#choice');
-    if (region ) {
+    if (region) {
         const regionChoice = new Choices((region), {
             searchEnabled: false,
         });
     }
-}   
- 
+}
+
+region();
 
 
+const inputField = document.querySelector('.choice-value');
+const dropdown = document.querySelector('.choice-list');
+const dropdownArray = [...document.querySelectorAll('choice-item')];
+console.log(typeof dropdownArray)
+let valueArray = [];
+dropdownArray.forEach(item => {
+    valueArray.push(item.textContent);
+});
 
-region();;
+const closeDropdown = () => {
+    dropdown.classList.remove('open');
+}
+
+inputField.addEventListener('input', () => {
+    dropdown.classList.add('open');
+    let inputValue = inputField.value.toLowerCase();
+    let valueSubstring;
+    if (inputValue.length > 0) {
+        for (let j = 0; j < valueArray.length; j++) {
+            if (!(inputValue.substring(0, inputValue.length) === valueArray[j].substring(0, inputValue.length).toLowerCase())) {
+                dropdownArray[j].classList.add('closed');
+            } else {
+                dropdownArray[j].classList.remove('closed');
+            }
+        }
+    } else {
+        for (let i = 0; i < dropdownArray.length; i++) {
+            dropdownArray[i].classList.remove('closed');
+        }
+    }
+});
+
+dropdownArray.forEach(item => {
+    item.addEventListener('click', (evt) => {
+        inputField.value = item.textContent;
+        dropdownArray.forEach(dropdown => {
+            dropdown.classList.add('closed');
+        });
+    });
+})
+
+inputField.addEventListener('focus', () => {
+    inputField.placeholder = 'Select Year';
+    dropdown.classList.add('open');
+    dropdownArray.forEach(dropdown => {
+        dropdown.classList.remove('closed');
+    });
+});
+
+inputField.addEventListener('blur', () => {
+    inputField.placeholder = 'Year';
+    dropdown.classList.remove('open');
+});
+
+document.addEventListener('click', (evt) => {
+    const isDropdown = dropdown.contains(evt.target);
+    const isInput = inputField.contains(evt.target);
+    if (!isDropdown && !isInput) {
+        dropdown.classList.remove('open');
+    }
+});;
 
 if (document.querySelector('.language')) {
     const languageToggler = document.querySelector('.current-lang');
